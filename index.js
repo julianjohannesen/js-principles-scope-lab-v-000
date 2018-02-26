85ab57d9630268fc26c5fdcdcb49a532fd66637f
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-26 13:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(name){
+  return name.toUpperCase();
+}
+console.log(upperCaseCustomerName(customerName));
+
+setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+console.log(setBestCustomer);
+console.log(bestCustomer);
