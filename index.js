b8f41c1c911fb4ebff6d2286a02df2d3b8bf3d7a
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-26 16:31:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function upperCaseCustomerName(){
 console.log(upperCaseCustomerName());
 
 function setBestCustomer(){
-  var bestCustomer = 'not bob';
+  bestCustomer = 'not bob';
   return bestCustomer;
 }
 console.log("Best customer: " + bestCustomer);
